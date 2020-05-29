75814a0b1605c60565ef620019db8cd10d0ecee6
Wilson Mauricio Hernandez <maohernandez@outlook.com>
2020-05-29 17:47:29 -0500

    Add Packages

---
+++ MvcScaffolding/MvcScaffolding.VS2015.1.0.12/tools/Controller/MvcScaffolding.Controller.ps1
@@ -0,0 +1,166 @@
+﻿[T4Scaffolding.ControllerScaffolder("Controller with read/write action and views, using EF data access code", Description = "Adds an ASP.NET MVC controller with views and data access code", SupportsModelType = $true, SupportsDataContextType = $true, SupportsViewScaffolder = $true)][CmdletBinding()]
+param(     
+	[parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)][string]$ControllerName,
+	[string]$ProjectName,
+	[string]$ModelType,
+    [string]$Project,
+    [string]$CodeLanguage,
+	[string]$DbContextType,
+	[string]$Area,
+	[string]$ViewScaffolder = "View",
+	[alias("MasterPage")]$Layout,
+ 	[alias("ContentPlaceholderIDs")][string[]]$SectionNames,
+	[alias("PrimaryContentPlaceholderID")][string]$PrimarySectionName,
+	[switch]$ReferenceScriptLibraries = $false,
+	[switch]$Repository = $false,
+	[switch]$NoChildItems = $false,
+	[string[]]$TemplateFolders,
+	[switch]$Force = $false,
+	[string]$ForceMode
+)
+
+# Interpret the "Force" and "ForceMode" options
+$overwriteController = $Force -and ((!$ForceMode) -or ($ForceMode -eq "ControllerOnly"))
+$overwriteFilesExceptController = $Force -and ((!$ForceMode) -or ($ForceMode -eq "PreserveController"))
+
+#Reescribo el proyecto destino para el controlador
+$Project = $ProjectName + ".Web"
+
+if(!$ProjectName){
+	throw "ProjectName is required."
+}
+
+# If you haven't specified a model type, we'll guess from the controller name
+if (!$ModelType) {
+	if ($ControllerName.EndsWith("Controller", [StringComparison]::OrdinalIgnoreCase)) {
+		# If you've given "PeopleController" as the full controller name, we're looking for a model called People or Person
+		$ModelType = [System.Text.RegularExpressions.Regex]::Replace($ControllerName, "Controller$", "", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+		$foundModelType = Get-ProjectType $ModelType -Project $Project -ErrorAction SilentlyContinue
+		if (!$foundModelType) {
+			$ModelType = [string](Get-SingularizedWord $ModelType)
+			$foundModelType = Get-ProjectType $ModelType -Project $Project -ErrorAction SilentlyContinue
+		}
+	} else {
+		# If you've given "people" as the controller name, we're looking for a model called People or Person, and the controller will be PeopleController
+		$ModelType = $ControllerName
+		$foundModelType = Get-ProjectType $ModelType -Project $Project -ErrorAction SilentlyContinue
+		if (!$foundModelType) {
+			$ModelType = [string](Get-SingularizedWord $ModelType)
+			$foundModelType = Get-ProjectType $ModelType -Project $Project -ErrorAction SilentlyContinue
+		}
+		if ($foundModelType) {
+			$ControllerName = [string](Get-PluralizedWord $foundModelType.Name) + "Controller"
+		}
+	}
+	if (!$foundModelType) { throw "Cannot find a model type corresponding to a controller called '$ControllerName'. Try supplying a -ModelType parameter value." }
+} else {
+	# If you have specified a model type
+	$foundModelType = Get-ProjectType $ModelType -Project $Project
+	if (!$foundModelType) { return }
+	if (!$ControllerName.EndsWith("Controller", [StringComparison]::OrdinalIgnoreCase)) {
+		$ControllerName = $ControllerName + "Controller"
+	}
+}
+Write-Host $Project
+Write-Host "Scaffolding $ControllerName..."
+
+#Reescribo el proyecto destino para el contexto de base de datos y repositorios
+$Project = $ProjectName + ".Infrastructure"
+if(!$DbContextType) { $DbContextType = [System.Text.RegularExpressions.Regex]::Replace((Get-Project $Project).Name, "[^a-zA-Z0-9]", "") + "Context" }
+if (!$NoChildItems) {
+	if ($Repository) {
+		Scaffold Repository -ModelType $foundModelType.FullName -ProjectName $ProjectName -DbContextType $DbContextType -Area $Area -Project $Project -CodeLanguage $CodeLanguage -Force:$overwriteFilesExceptController
+	} else {
+		$dbContextScaffolderResult = Scaffold DbContext -ModelType $foundModelType.FullName -DbContextType $DbContextType -Area $Area -Project $Project -CodeLanguage $CodeLanguage
+		$foundDbContextType = $dbContextScaffolderResult.DbContextType
+		if (!$foundDbContextType) { return }
+	}
+}
+if (!$foundDbContextType) { $foundDbContextType = Get-ProjectType $DbContextType -Project $Project }
+if (!$foundDbContextType) { return }
+
+$primaryKey = Get-PrimaryKey $foundModelType.FullName -Project $Project -ErrorIfNotFound
+if (!$primaryKey) { return }
+
+#Reescribo el proyecto destino para las vistas
+$Project = $ProjectName + ".Web"
+
+$outputPath = Join-Path Controllers $ControllerName
+# We don't create areas here, so just ensure that if you specify one, it already exists
+if ($Area) {
+	$areaPath = Join-Path Areas $Area
+	if (-not (Get-ProjectItem $areaPath -Project $Project)) {
+		Write-Error "Cannot find area '$Area'. Make sure it exists already."
+		return
+	}
+	$outputPath = Join-Path $areaPath $outputPath
+}
+
+# Prepare all the parameter values to pass to the template, then invoke the template with those values
+$serviceName = $foundModelType.Name + "Service"
+$defaultNamespace = (Get-Project $Project).Properties.Item("DefaultNamespace").Value
+$modelTypeNamespace = [T4Scaffolding.Namespaces]::GetNamespace($foundModelType.FullName)
+$controllerNamespace = [T4Scaffolding.Namespaces]::Normalize($defaultNamespace + "." + [System.IO.Path]::GetDirectoryName($outputPath).Replace([System.IO.Path]::DirectorySeparatorChar, "."))
+$areaNamespace = if ($Area) { [T4Scaffolding.Namespaces]::Normalize($defaultNamespace + ".Areas.$Area") } else { $defaultNamespace }
+$dbContextNamespace = $foundDbContextType.Namespace.FullName
+$serviceNamespace = [T4Scaffolding.Namespaces]::Normalize($ProjectName + ".Manager.Interfaces")
+$modelTypePluralized = Get-PluralizedWord $foundModelType.Name
+$relatedEntities = [Array](Get-RelatedEntities $foundModelType.FullName -Project $project)
+if (!$relatedEntities) { $relatedEntities = @() }
+
+$templateName = if($Repository) { "ControllerWithRepository" } else { "ControllerWithContext" }
+Add-ProjectItemViaTemplate $outputPath -Template $templateName -Model @{
+	ControllerName = $ControllerName;
+	ModelType = [MarshalByRefObject]$foundModelType; 
+	PrimaryKey = [string]$primaryKey; 
+	DefaultNamespace = $defaultNamespace; 
+	AreaNamespace = $areaNamespace; 
+	DbContextNamespace = $dbContextNamespace;
+	ServiceNamespace = $serviceNamespace;
+	ModelTypeNamespace = $modelTypeNamespace; 
+	ControllerNamespace = $controllerNamespace; 
+	DbContextType = [MarshalByRefObject]$foundDbContextType;
+	Service = $serviceName; 
+	ModelTypePluralized = [string]$modelTypePluralized; 
+	RelatedEntities = $relatedEntities;
+} -SuccessMessage "Added controller {0}" -TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$overwriteController
+
+if (!$NoChildItems) {
+	$controllerNameWithoutSuffix = [System.Text.RegularExpressions.Regex]::Replace($ControllerName, "Controller$", "", [System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+	if ($ViewScaffolder) {
+		Scaffold Views -ViewScaffolder $ViewScaffolder -Controller $controllerNameWithoutSuffix -ModelType $foundModelType.FullName -Area $Area -Layout $Layout -SectionNames $SectionNames -PrimarySectionName $PrimarySectionName -ReferenceScriptLibraries:$ReferenceScriptLibraries -Project $Project -CodeLanguage $CodeLanguage -Force:$overwriteFilesExceptController
+	}
+}
+
+
+#services interfaces
+$Project = $ProjectName + ".Manager"
+$outputPath = Join-Path "Interfaces" ("I" + $foundModelType.Name + "Service")
+Add-ProjectItemViaTemplate $outputPath -Template IService -Model @{
+	ModelType = [MarshalByRefObject]$foundModelType; 
+	PrimaryKey = [string]$primaryKey; 
+	DefaultNamespace = $defaultNamespace;
+	ModelTypeNamespace = $modelTypeNamespace; 
+	ModelTypePluralized = [string]$modelTypePluralized; 
+	DbContextNamespace = $foundDbContextType.Namespace.FullName;
+	DbContextType = [MarshalByRefObject]$foundDbContextType;
+	ProjectName = $ProjectName;
+} -SuccessMessage "Added iservice '{0}'" -TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
+Write-Host $TemplateFolders $Project $outputPath $foundModelType $primaryKey $defaultNamespace $modelTypeNamespace $modelTypePluralized $foundDbContextType.Namespace.FullName
+
+
+#services implementation
+$Project = $ProjectName + ".Manager"
+$outputPath = Join-Path "Implementations" ($foundModelType.Name + "Service")
+Add-ProjectItemViaTemplate $outputPath -Template Service -Model @{
+	ModelType = [MarshalByRefObject]$foundModelType; 
+	PrimaryKey = [string]$primaryKey; 
+	DefaultNamespace = $defaultNamespace;
+	ModelTypeNamespace = $modelTypeNamespace; 
+	ModelTypePluralized = [string]$modelTypePluralized; 
+	DbContextNamespace = $foundDbContextType.Namespace.FullName;
+	DbContextType = [MarshalByRefObject]$foundDbContextType;
+	ProjectName = $ProjectName;
+} -SuccessMessage "Added service '{0}'" -TemplateFolders $TemplateFolders -Project $Project -CodeLanguage $CodeLanguage -Force:$Force
+
+Write-Host $outputPath
